490a64b169f81d1f6a8ee3c689d768b43aa9232c
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-02-26 14:46:58 -0400

    basicos de js em html

---
+++ exercios/aula14/acoes.js
@@ -0,0 +1,8 @@
+let resp = window.document.getElementById('saida')
+
+function acao1() {
+    resp.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+function acao2() {
+    resp.innerHTML += '<p>Clicou no segundo botão</p>'
+}
