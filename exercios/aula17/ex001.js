c58aa96b297d2404c1d4d4a4a909a01d63dea6e2
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-02-27 14:50:48 -0400

    ex de funcão e conficões

---
+++ exercios/aula17/ex001.js
@@ -0,0 +1,5 @@
+var vel = 60.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapasso a velocidade permitida.Multado!')
+}
---
+++ exercios/aula17/ex002.js
@@ -0,0 +1,9 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Brasileiro!')
+}else if (país == 'EUA') {
+    console.log('Gringo')
+}else {
+    console.log('Você é um indigente')
+}
