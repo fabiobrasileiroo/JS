9e93e6f85c9af33c0736a469955d2d95462157bc
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-03-05 13:31:28 -0400

    script

--- desafio2/script.js
+++ desafio2/script.js
@@ -4,19 +4,41 @@ function verificar() {
     var fAno = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     var msg = document.getElementById('erro')
-    if (fAno.value.length == 0 || fAno.value > ano ) {
+    if (fAno.value.length == 0 || Number(fAno.value) > ano ) {
         msg.innerHTML = '[Erro] Verificar os dados e tente novamente!'
         erro.style.color = '#D2122E'
     } else {
-        var fsex = document.getElementByName('radsex')
-        var idade = ano - Number(fano.value)
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
         var gênero = ''
-        if (fsec[0].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
             gênero = 'Homem'
-        } else if (fsex[1].checked) {
+            if (idade >=0 && idade < 10){//criança
+                img.setAttribute('src','foto-bebe-m.png' )
+            } else if (idade < 21) {//jovem
+               img.setAttribute('src', 'foto-jovem-m.png') 
+            } else if (idade < 50) {//adulto
+                img.setAttribute('src','foto-adulto-m.png')
+            } else {//idoso
+                img.setAttribute('src','foto-idoso-m.png')
+            }
+
+        } else if (fsex[1].checked) 
             gênero = 'Mulher'
-        } 
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
-    }
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src','foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src','foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+      
+    }  
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    res.appendChild(img)
 }
