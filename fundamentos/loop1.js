354fccb97944088f3b1307d0f69d54307966b006
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-03-23 20:00:29 -0400

    obj e fun

---
+++ fundamentos/loop1.js
@@ -0,0 +1,16 @@
+var i = 0
+if(i < 10) {
+    i++
+    console.log(i)
+}
+console.log(i)
+
+const funcs = []
+for (var i = 0; i < 10; i++) {
+    funcs.push(function() {
+        console.log(i)
+    })
+} 
+
+funcs[2]()
+funcs[8]()
