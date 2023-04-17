5ce1b0ccbd3c1d68727449fb31bdf9424bcc46a3
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-04-17 19:07:14 -0400

    a

--- fundamentos/desafio_2.js
+++ fundamentos/desafio_2.js
@@ -11,3 +11,15 @@ console.log(!(a==b))
 console.log(a!=b)
 console.log(a < b)
 console.log(-1 < -7)
+
+let w = 'um palavra'
+w += ', aqui mais outra 3'
+console.log(w)
+console.log('Bom dia \n Tudo bem?')//n nova linha
+console.log('ola \t tab')
+
+console.log("Lista de aprovados \n=========================")
+let texto = "1.Ana Silva"
+texto += "\n2. Pedro Alvuquerque"
+texto = texto +"\n3. Guilherme"
+console.log(texto)
