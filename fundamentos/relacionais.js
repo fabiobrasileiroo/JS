d18b47a69f6e36e9c56bf3192c1a2f87206ab395
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-07-29 12:11:57 -0400

    treinando

---
+++ fundamentos/relacionais.js
@@ -0,0 +1,10 @@
+const d1 = new Date(0)
+const d2 = new Date(0)
+
+console.log(d1==d2)
+console.log(d1==d2);
+console.log(d1.getDate() === d2.getDate())
+console.log(d1.getDate() == d2.getDate())
+
+console.log(undefined == null)
+console.log(undefined === null)
